a0055590c3ec2a0f40ebf284284eaf493a1cab1a
Brody May <brodymay2019@outlook.com>
2023-02-14 12:21:07 -0500

    finished

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car 
+    { 
+        
+    
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string noise) 
+        {
+            Console.WriteLine($"The {Make}'s engine sounds like {noise}");
+        }
+
+        public void MakeHonkNoise() 
+        {
+            Console.WriteLine($"The {Make}'s honk sounds like {HonkNoise}");
+        }
+    }
+
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -17,7 +17,37 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            CarLot myCarlot= new CarLot();
+            
+            var car1 = new Car();
+            car1.Make = "Toyota";
+            car1.Model = "Corola";
+            car1.Year = 2022;
+            car1.IsDriveable= true;
+            car1.EngineNoise = "vroooom";
+            car1.HonkNoise = "beep";
 
+            myCarlot.ListOfCars.Add(car1);
+
+            var car2 = new Car();
+            car2.Make = "Ford";
+            car2.Model = "Fusion";
+            car2.Year = 2006;
+            car2.IsDriveable= true;
+            car2.EngineNoise = "vreer";
+            car2.HonkNoise = "weep";
+
+            myCarlot.ListOfCars.Add(car2);
+
+            var car3 = new Car();
+            car3.Make = "BMW";
+            car3.Model = "X1";
+            car3.Year = 2021;
+            car3.IsDriveable= true;
+            car3.EngineNoise = "arrggg";
+            car3.HonkNoise = "meep";
+
+            myCarlot.ListOfCars.Add(car3);
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
